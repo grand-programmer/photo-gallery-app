c815ed05edc36914afa41cd0e8a782dd2fc33951
surat Abdullayev <suratbek92@gmail.com>
2025-01-02 17:41:40 +0500

    Added Blob type

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -51,7 +51,7 @@ export const usePhotoGallery = () => {
     };
 
     const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-        let base64Data: string;
+        let base64Data: string | Blob;
         // "hybrid" will detect mobile - iOS or Android
         if (isPlatform('hybrid')) {
             const file = await Filesystem.readFile({
